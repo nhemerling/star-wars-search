2b1aaf38c8ead2bb6c2ce9ff7b563b0185bc31bf
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-07-31 08:51:43 -0700

    add header that functins as link back to home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Header from './Header/Header';
+import Main from './Main/Main';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +11,8 @@ class App extends Component {
   render() {  
     return (
       <main className='App'>
-        {/* content goes here */}
+        <Header />
+        <Main />
       </main>
     );
   }
