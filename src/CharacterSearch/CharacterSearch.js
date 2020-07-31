0ee9575dad3ed094796f3cb0b274580b9ce2a706
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-07-31 11:56:21 -0700

    now should have loading screen while awaiting results

--- src/CharacterSearch/CharacterSearch.js
+++ src/CharacterSearch/CharacterSearch.js
@@ -1,40 +1,14 @@
 import React, { Component } from 'react';
-import config from '../config'
 import './CharacterSearch.css';
 
 export default class CharacterSearch extends Component {
 
-    handleSearch = (event) => {
-        event.preventDefault();
-        //get the search terms entered by the user
-        const searchTerms = event.target.searchInput.value;
-        // console.log(searchTerms);
-        // console.log(config.API_ENDPOINT);
-        //make a fetch call with the search terms
-        fetch(`${config.API_ENDPOINT}/people/?search=${searchTerms}`)
-            .then(res => {
-                if (!res.ok) {
-                    return res.json().then(e => Promise.reject(e))
-                }
-                return res.json()
-            })
-            .then(data => {
-                // console.log(data);
-                // console.log(data.results);
-                // console.log(data.results[0]);
-                // console.log(data.results[0].name)
-                // console.log('is this an array?', typeof data.results);
-                this.props.showResults(data.results)
-            })
-            .catch(error => {
-                console.error({ error })
-            })
-    }
+    
 
     render() {
         return (
             <div className="Search">
-                <form className="search-form" onSubmit={e => this.handleSearch(e)}>
+                <form className="search-form" onSubmit={e => this.props.onSearch(e)}>
                     <label htmlFor="searchInput">The Galaxy awaits your search terms:</label>
                     <input type="text" name="searchInput" id="searchInput" required></input>
                     <button type="submit">Search</button>
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
 import CharacterSearch from '../CharacterSearch/CharacterSearch';
 import Results from '../Results/Results';
+import config from '../config';
 import './Main.css';
 
 export default class Main extends Component {
     state = {
-        searchResults: []
+        searchResults: [],
+        loading: false
     }
 
     handleShowResults = (searchData) => {
         this.setState({
-            searchResults: searchData
+            searchResults: searchData,
+            loading: false
         });
+    }
+
+    handleSearch = (event) => {
+        event.preventDefault();
+        //get the search terms entered by the user
+        const searchTerms = event.target.searchInput.value;
+        //empty results and provide loading screen
+        this.setState({
+            searchResults: [],
+            loading: true
+        });
+        //make a fetch call with the search terms
+        fetch(`${config.API_ENDPOINT}/people/?search=${searchTerms}`)
+            .then(res => {
+                if (!res.ok) {
+                    return res.json().then(e => Promise.reject(e))
+                }
+                return res.json()
+            })
+            .then(data => {
+                this.handleShowResults(data.results)
+            })
+            .catch(error => {
+                console.error({ error })
+            })
     }
 
     render() {
         return(
             <main>
-                <CharacterSearch showResults={this.handleShowResults} />
+                <CharacterSearch showResults={this.handleShowResults} onSearch={this.handleSearch}/>
                 <Results results={this.state.searchResults} />
+                {this.state.loading && <h3>searching the galaxy...</h3>}
             </main>
         );
     }
