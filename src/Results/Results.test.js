39a18d3580dea3c4c10d4400037ab7a32217c361
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-07-31 14:47:58 -0700

    add test suite to Results

--- src/Results/Results.test.js
+++ src/Results/Results.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Results from './Results';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Results />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Results component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Results />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the UI as expected with empty results array', () => {
+    const tree = renderer
+      .create(<Results
+                results={[]} type="people" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders the UI as expected with empty results array and type set as vehicles', () => {
+    const tree = renderer
+      .create(<Results
+                results={[]} type="vehicles" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders the UI as expected with no props', () => {
+    const tree = renderer
+      .create(<Results />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
